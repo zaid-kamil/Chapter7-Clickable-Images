2d58b894187b3d01b374080209e336b39ac81a75
Zaid kamil <xaidmetamorphos@gmail.com>
2021-01-29 12:19:54 +0530

    input controls

--- app/src/main/java/com/example/chapter7cartooncafe/OrderActivity.java
+++ app/src/main/java/com/example/chapter7cartooncafe/OrderActivity.java
@@ -3,6 +3,8 @@ package com.example.chapter7cartooncafe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        switch (view.getId()) {
+            case R.id.sameday:
+                Toast.makeText(this, "you selected same day delivery", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nextday:
+                Toast.makeText(this, "you selected next day delivery", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.pickup:
+                Toast.makeText(this, "you selected Pickup", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
