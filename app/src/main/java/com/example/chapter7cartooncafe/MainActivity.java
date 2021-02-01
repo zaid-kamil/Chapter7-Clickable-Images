207b4159e78403ada19facafff38c238be291686
Zaid kamil <xaidmetamorphos@gmail.com>
2021-02-01 11:50:30 +0530

    finished

--- app/src/main/java/com/example/chapter7cartooncafe/MainActivity.java
+++ app/src/main/java/com/example/chapter7cartooncafe/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.chapter7cartooncafe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -16,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(v->{
+        fab.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, OrderActivity.class);
             startActivity(intent);
         });
@@ -25,14 +28,42 @@ public class MainActivity extends AppCompatActivity {
     public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
+
     public void showDonutOrder(View view) {
         displayToast(getString(R.string.donut_order_message));
     }
 
-    public void showIceCreamOrder(View view){
+    public void showIceCreamOrder(View view) {
         displayToast(getString(R.string.ice_cream_order_message));
     }
+
     public void showFroyoOrder(View view) {
         displayToast(getString(R.string.froyo_order_message));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_contact:
+                displayToast("show the contacts");
+                break;
+            case R.id.action_favorite:
+                displayToast("favorites items to be loaded");
+                break;
+            case R.id.action_order:
+                Intent i = new Intent(this, OrderActivity.class);
+                startActivity(i);
+                break;
+            case R.id.action_status:
+                displayToast("Show the order status");
+                break;
+        }
+        return true;
     }
 }
